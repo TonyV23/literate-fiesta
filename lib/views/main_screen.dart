0aa162b60bb192ca2de5e7a3e7213b7e9b81cf7d
viper23 <htonnycarlos@gmail.com>
2025-07-24 19:19:56 +0200

    feat: implement main screen with bottom navigation and theme integration
    This commit introduces the `MainScreen` widget, which serves as the primary interface for the application.
    
    Key features include:
    - Integration with `NavigationController` to manage and display different screens (Home, Shopping, Wishlist, Account) using an `IndexedStack` and `AnimatedSwitcher` for smooth transitions.
    - Integration with `ThemeController` to dynamically update the screen's background color based on the selected theme.
    - Implementation of a `CustomBottomNavbar` for navigating between the different sections of the app.

--- lib/views/main_screen.dart
+++ lib/views/main_screen.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:pablo/controllers/navigation_controller.dart';
+import 'package:pablo/controllers/theme_controller.dart';
+import 'package:pablo/views/account_screen.dart';
+import 'package:pablo/views/home_screen.dart';
+import 'package:pablo/views/shopping_screen.dart';
+import 'package:pablo/views/widgets/custom_bottom_navbar.dart';
+import 'package:pablo/views/wishlist_screen.dart';
+
 
 class MainScreen extends StatelessWidget {
-  const MainScreen({super.key});
+    const MainScreen({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Main screen"),
-      ),
-    );
-  }
+    @override
+    Widget build(BuildContext context) {
+
+      final NavigationController navigationController = Get.find<NavigationController>();
+
+        return GetBuilder<ThemeController>(
+            builder: (themeController) => Scaffold( 
+                backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                body: AnimatedSwitcher(
+                    duration: Duration(milliseconds: 200), 
+                    child: Obx(() => IndexedStack(
+                            key: ValueKey(navigationController.currentIndex.value),
+                            index: navigationController.currentIndex.value,
+                            children: [
+                                HomeScreen(),
+                                ShoppingScreen(),
+                                WishlistScreen(),
+                                AccountScreen()
+                            ],
+                        )),),
+              bottomNavigationBar: CustomBottomNavbar(),
+            ));
+    }
 }

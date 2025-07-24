8cbf58e1a650a23cbdbb64b3b75ce959e78649d3
viper23 <htonnycarlos@gmail.com>
2025-07-24 14:00:51 +0200

    enhance ForgotPasswordScreen with UI elements and password reset functionality
    This commit introduces a user interface for the `ForgotPasswordScreen`, including:
    - A back button for navigation.
    - Text fields for entering the email address.
    - A "Send reset link" button to initiate the password reset process.
    - A success dialog that appears after the reset link is sent.
    
    The screen now allows users to input their email and request a password reset link. Styling is adapted based on the current theme (light/dark).

--- lib/views/forgot_password_screen.dart
+++ lib/views/forgot_password_screen.dart
@@ -1,14 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:pablo/views/signin_screen.dart';
+import 'package:pablo/views/widgets/custom_textfield.dart';
+
+import '../utils/app_textstyles.dart';
 
 class ForgotPasswordScreen extends StatelessWidget {
-  const ForgotPasswordScreen({super.key});
+    ForgotPasswordScreen({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(child: SingleChildScrollView(
-        child: SafeArea(child: Text("")),
-      ),),
-    );
-  }
+    final TextEditingController _emailController = TextEditingController();
+
+    @override
+    Widget build(BuildContext context) {
+
+        final isDark = Theme.of(context).brightness == Brightness.dark;
+
+        return Scaffold(
+            body: SafeArea(child: SingleChildScrollView(
+                    padding: EdgeInsets.all(24),
+                    child: SafeArea(child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                                // back button
+                                IconButton(onPressed: () => Get.back(), icon: Icon(Icons.arrow_back_ios), color: isDark ? Colors.white : Colors.black,),
+                                Text("Reset Password", style: AppTextstyles.withColor(AppTextstyles.h1, Theme.of(context).textTheme.bodyLarge!.color!),),
+                                SizedBox(height: 8,),
+                                Text("Enter your email to reset your password",
+                                    style: AppTextstyles.withColor(AppTextstyles.bodLarge, isDark ? Colors.grey[400]! : Colors.grey[600]!)),
+
+                                SizedBox(height: 40,),
+                                CustomTextfield(
+                                    label: "Email",
+                                    prefixIcon: Icons.email_outlined,
+                                    keyboardType: TextInputType.emailAddress,
+                                    controller: _emailController,
+                                    validator: (value) {
+                                        if (value == null || value.isEmpty) {
+                                            return "Please enter your email";
+                                        }
+                                        if (!GetUtils.isEmail(value)) {
+                                            return "Please enter a valid email";
+                                        }
+                                        return null;
+                                    },
+                                ),
+                                const SizedBox(height: 24,),
+
+                                // sign in button
+                                SizedBox(
+                                    width: double.infinity,
+                                    child: ElevatedButton(
+                                        onPressed: () {
+                                            showSuccessDialog(context);
+                                        },
+                                        style: ElevatedButton.styleFrom(
+                                            backgroundColor: Theme.of(context).primaryColor,
+                                            padding: EdgeInsets.symmetric(vertical: 16),
+                                            shape: RoundedRectangleBorder(
+                                                borderRadius: BorderRadius.circular(12)
+                                            )
+                                        ),
+                                        child: Text("Send reset link", style: AppTextstyles.withColor(AppTextstyles.buttonMedium, Colors.white),)),
+                                ),
+
+                                const SizedBox(height: 24,),
+
+                            ],
+                        )),
+                ),),
+        );
+    }
+}
+
+// show pop up dialog after sending link password
+void showSuccessDialog(BuildContext context) {
+    Get.dialog(AlertDialog(
+            title: Text("Check Your Email", style: AppTextstyles.h3,),
+            content: Text("We have sent password recovery instructions to your email.", 
+                style: AppTextstyles.bodMedium,),
+            actions: [
+                TextButton(onPressed: () => Get.back(), child: Text("OK", style: AppTextstyles.withColor(AppTextstyles.buttonMedium, Theme.of(context).primaryColor),))
+            ],
+        ));
 }
